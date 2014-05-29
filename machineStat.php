a89cdfae2fc4d56eb68f058bd1da717d2e8c00b3
Andrew Fugier <fugier@gmail.com>
2014-05-29 12:57:59 -0700

    Adding machineStat.php file

---
+++ machineStat.php
@@ -0,0 +1,14 @@
+<?php
+print("{\"m\":\"" . $_GET["machine"] . "\",\"o\":\"" . inOS($_GET["machine"]) . "\"}");
+
+function inOS($machine) {
+	error_reporting(E_ERROR);
+	if (fsockopen($machine, 22, $errno, $errstr, .01)) { return "Linux"; }
+	elseif (fsockopen($machine, 135, $errno, $errstr, .01)) { return "Windows8"; }
+	elseif (fsockopen($machine, 3389, $errno, $errstr, .01)) { return "Windows"; }
+	elseif (fsockopen($machine, 1022, $errno, $errstr, .01)) { return "Server"; }
+	elseif (fsockopen($machine, 554, $errno, $errstr, .01)) { return "Camera"; }
+	else { return "Other"; }
+	error_reporting(E_WARNING);
+}
+?>
