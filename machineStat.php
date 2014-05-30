e0d99df48f61eda7ebfd521c725b5bea9c7f78fd
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-05-30 15:42:36 -0700

    Properly indent machineStat.php.

--- machineStat.php
+++ machineStat.php
@@ -1,14 +1,14 @@
 <?php
-print("{\"os\":\"" . inOS($_GET["machine"]) . "\"}");
+    print("{\"os\":\"" . inOS($_GET["machine"]) . "\"}");
 
-function inOS($machine) {
-	error_reporting(E_ERROR);
-	if (fsockopen($machine, 22, $errno, $errstr, .01)) { return "Linux"; }
-	elseif (fsockopen($machine, 135, $errno, $errstr, .01)) { return "Windows8"; }
-	elseif (fsockopen($machine, 3389, $errno, $errstr, .01)) { return "Windows"; }
-	elseif (fsockopen($machine, 1022, $errno, $errstr, .01)) { return "Server"; }
-	elseif (fsockopen($machine, 554, $errno, $errstr, .01)) { return "Camera"; }
-	else { return "Other"; }
-	error_reporting(E_WARNING);
-}
+    function inOS($machine) {
+        error_reporting(E_ERROR);
+        if (fsockopen($machine, 22, $errno, $errstr, .01)) { return "Linux"; }
+        elseif (fsockopen($machine, 135, $errno, $errstr, .01)) { return "Windows8"; }
+        elseif (fsockopen($machine, 3389, $errno, $errstr, .01)) { return "Windows"; }
+        elseif (fsockopen($machine, 1022, $errno, $errstr, .01)) { return "Server"; }
+        elseif (fsockopen($machine, 554, $errno, $errstr, .01)) { return "Camera"; }
+        else { return "Other"; }
+        error_reporting(E_WARNING);
+    }
 ?>
